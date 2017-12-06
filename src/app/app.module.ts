0117aa889c27a82710578391edbac61d541cbc7f
DaanTacoSchool <djmessen@avans.nl>
2017-12-06 13:49:08 +0100

    added empty cardcomponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DecksComponent } from './decks/decks.component';
+import { CardComponent } from './card/card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DecksComponent
+    DecksComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
